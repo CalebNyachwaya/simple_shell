194c909abf3b46778d3cae9832aa4cf21eb60dcb
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-02-22 04:29:04 -0800

    edit shell.c

--- shell.c
+++ shell.c
@@ -7,7 +7,7 @@ void sig_handler(int sig_num)
 {
 	if (sig_num == SIGINT)
 	{
-		_puts("\n#$ ");
+		_puts("\n#cisfun$$ ");
 	}
 }
 
@@ -36,7 +36,7 @@ void _EOF(int len, char *buff)
 void _isatty(void)
 {
 	if (isatty(STDIN_FILENO))
-		_puts("$ ");
+		_puts("#cisfun$ ");
 }
 /**
  * main - Shell
