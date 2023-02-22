326fbd057fe535af12c9de7ee2dc6a5888a85d92
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2023-02-22 14:42:36 +0000

    trial

--- shell.c
+++ shell.c
@@ -1,87 +1,76 @@
-#include "shell.h"
-/**
- * sig_handler - checks if Ctrl C is pressed
- * @sig_num: int
- */
-void sig_handler(int sig_num)
-{
-	if (sig_num == SIGINT)
-	{
-		_puts("\n#cisfun$ ");
-	}
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-/**
-* _EOF - handles the End of File
-* @len: return value of getline function
-* @buff: buffer
- */
-void _EOF(int len, char *buff)
+extern char **environ; 
+
+int main(int argc, char **argv)
 {
-	(void)buff;
-	if (len == -1)
+	char *str = NULL;
+	size_t n;
+	char *stkn;
+	pid_t mypid;
+	int status, i;
+
+	while (1)
 	{
-		if (isatty(STDIN_FILENO))
-		{
-			_puts("\n");
-			free(buff);
-		}
+	printf("$ ");
+	getline(&str, &n, stdin);
+
+	stkn = strtok(str, " \n");/*solves the issue of executing command*/
+
+	char **arr = malloc(sizeof(char *) * 32);
+
+	arr[0] = stkn;
+/*
+	if (strcmp(arr[0], "exit") == 0)
 		exit(0);
-	}
-}
-/**
-  * _isatty - verif if terminal
-  */
+*/
+	i = 1;
 
-void _isatty(void)
-{
-	if (isatty(STDIN_FILENO))
-		_puts("#cisfun$ ");
-}
-/**
- * main - Shell
- * Return: 0 on success
- */
-int main(void)
-{
-	ssize_t len = 0;
-	char *buff = NULL, *value, *pathname, **arv;
-	size_t size = 0;
-	list_path *head = '\0';
-	void (*f)(char **);
-
-	signal(SIGINT, sig_handler);
-	while (len != EOF)
+	while (stkn != NULL)
 	{
-		_isatty();
-		len = getline(&buff, &size, stdin);
-		_EOF(len, buff);
-		arv = splitstring(buff, " \n");
-		if (!arv || !arv[0])
-			execute(arv);
-		else
+		stkn = strtok(NULL, " \n");
+		arr[i] = stkn;
+		i++;
+	}
+	if (strcmp(arr[0], "exit") == 0 && (arr[1] == NULL))
+		exit(0);
+
+	if (strcmp(arr[0], "exit") == 0 && (arr[1] == NULL))
+	{
+		i = 0;
+		while (environ[i] != NULL)
 		{
-			value = _getenv("PATH");
-			head = linkpath(value);
-			pathname = _which(arv[0], head);
-			f = checkbuild(arv);
-			if (f)
-			{
-				free(buff);
-				f(arv);
-			}
-			else if (!pathname)
-				execute(arv);
-			else if (pathname)
-			{
-				free(arv[0]);
-				arv[0] = pathname;
-				execute(arv);
-			}
+			printf("%s\n", environ[i]);
+			i++;
+		}                
+		continue;
+	}
+
+	mypid = fork();
+	if (mypid == -1)
+	{
+		perror("Error");
+		return (1);
+	}
+	else if (mypid == 0)/*solves the issue of exiting*/
+	{
+		if (execve(arr[0], arr, NULL) == -1)
+		{
+			perror(argv[0]);
+			return (0);
 		}
 	}
-	free_list(head);
-	freearv(arv);
-	free(buff);
+	else
+	{
+		wait(NULL);/*suspends the parent from action until the child finishes*/
+	}
+	}
+
+	free(str);
 	return (0);
 }
